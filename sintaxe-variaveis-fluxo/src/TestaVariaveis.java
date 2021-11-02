d0b78dc748227f7ddf1289b8a0d9ea475b1d9704
Lucas Santiago <slucassantiago@hotmail.com>
2021-11-01 22:31:51 -0300

    Hello World

---
+++ sintaxe-variaveis-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,16 @@
+public class TestaVariaveis {
+
+		public static void main(String[] args) {
+			
+			int idade = 24;
+			System.out.println("Novo teste");	
+			System.out.println("Idade: "+idade+" anos");
+			
+			//Conversões
+			double valor = 7.5;
+			int total = (int) valor;
+			System.out.println();
+			System.out.println(total);
+			
+		}
+}
