1ff39aa763e315dd19a2f9a90873f4064bc111de
Lucas Santiago <slucassantiago@hotmail.com>
2021-11-04 19:45:41 -0300

    IF

---
+++ sintaxe-variaveis-fluxo/src/testarCondicional.java
@@ -0,0 +1,15 @@
+
+public class testarCondicional {
+
+	public static void main(String[] args) {
+		System.out.println("Teste condicionais");
+		int idade = 15;
+		
+		if (idade >= 18) {
+			System.out.println("Você tem mais de 18 anos!");
+		}
+		else if (idade <= 17) {
+			System.out.println("Você tem menos de 18 anos!");
+		}
+	}
+}
